710100e0106e66c2ddafa4d70cd10a058e238076
Oleksandr_Gneushev <galexing@gmail.com>
2021-06-14 17:19:57 +0200

    finish with calculator

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -1,7 +1,108 @@
-import { Container } from "./Styled";
+import { useState } from "react";
+import { Container, Previous, Screen, Current, Button } from "./Styled";
 
 const Calculator = () => {
-  return <Container />;
+  const [previous, setPrevious] = useState("");
+  const [current, setCurrent] = useState("");
+  const [operation, setOperation] = useState("");
+
+  const appendValue = ({ target }) => {
+    const value = target.innerHTML;
+    if (value === "." && current.includes(value)) return;
+    setCurrent(current + value);
+  };
+
+  const handleDelete = () => {
+    return current.length <= 1
+      ? setCurrent("")
+      : setCurrent(current.slice(0, -1));
+  };
+
+  const handleAllClear = () => {
+    setCurrent("");
+    setPrevious("");
+    setOperation("");
+  };
+
+  const handleOperation = ({ target }) => {
+    if (!current) return;
+    if (previous) {
+      const value = compute().toString();
+      setPrevious(value);
+    } else {
+      setPrevious(current);
+    }
+
+    setCurrent("");
+    setOperation(target.innerHTML);
+  };
+
+  const equal = () => {
+    const value = compute().toString();
+    if (value === undefined || value === null) return;
+
+    setCurrent(value);
+    setPrevious("");
+    setOperation("");
+  };
+
+  const compute = () => {
+    // let result;
+    // let previousNumber = parseFloat(previous);
+    // let currentNumber = parseFloat(current);
+
+    // if (isNaN(previousNumber) || isNaN(currentNumber)) return;
+
+    if (operation === "÷") return previous / current;
+    if (operation === "×") return previous * current;
+    if (operation === "+") return previous + current;
+    if (operation === "-") return previous - current;
+  };
+
+  return (
+    <Container>
+      <Screen>
+        <Previous>
+          {previous} {operation}
+        </Previous>
+        <Current>{current}</Current>
+      </Screen>
+      <Button onClick={handleAllClear} gridSpan={2} control>
+        AC
+      </Button>
+      <Button onClick={handleDelete} control>
+        DEL
+      </Button>
+      <Button onClick={handleOperation} operation>
+        ÷
+      </Button>
+      <Button onClick={appendValue}>7</Button>
+      <Button onClick={appendValue}>8</Button>
+      <Button onClick={appendValue}>9</Button>
+      <Button onClick={handleOperation} operation>
+        ×
+      </Button>
+      <Button onClick={appendValue}>4</Button>
+      <Button onClick={appendValue}>5</Button>
+      <Button onClick={appendValue}>6</Button>
+      <Button onClick={handleOperation} operation>
+        +
+      </Button>
+      <Button onClick={appendValue}>1</Button>
+      <Button onClick={appendValue}>2</Button>
+      <Button onClick={appendValue}>3</Button>
+      <Button onClick={handleOperation} operation>
+        -
+      </Button>
+      <Button onClick={appendValue} period>
+        .
+      </Button>
+      <Button onClick={appendValue}>0</Button>
+      <Button onClick={equal} gridSpan={2} equal>
+        =
+      </Button>
+    </Container>
+  );
 };
 
 export default Calculator;
